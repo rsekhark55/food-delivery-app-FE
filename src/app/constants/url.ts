960e667ece5a9c4a3c52802f2415730c2435dbe6
rsekhark55 <146623822+rsekhark55@users.noreply.github.com>
2024-02-17 21:44:16 -0600

    Update url.ts

--- src/app/constants/url.ts
+++ src/app/constants/url.ts
@@ -3,5 +3,5 @@
 // export const API_URL_FC ='http://localhost:9092';
 // export const API_URL_UD ='http://localhost:9093';
 
-export const K8ExternalIp = 'http://k8s-default-awsingre-3d4b7f90d4-122545836.eu-west-3.elb.amazonaws.com';
+export const K8ExternalIp = 'http://k8s-default-awsingre-9aa4c55379-103286389.us-east-1.elb.amazonaws.com';
 
